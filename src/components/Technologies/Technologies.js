b3f34dfd23cb0cde0e75b69cd388151a0830ffa0
Abdulaziz <abdulazizfaactory@gmail.com>
2022-01-11 20:29:31 +0500

    technologies section is done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiReact, DiZend, DiPhp } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <Section id="#tech">
     <SectionDivider/>
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I 've been working with a range of technologies in the web development.
+      From Back-end to Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPhp size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            PHP and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Tools like Figma 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
